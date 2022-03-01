256b86e2ba08be587f48ab96e8fe5d2ceef9d693
Krystian Tomczyk <krynio340@gmail.com>
2022-03-01 17:41:19 +0100

    cv button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var cvButton = document.getElementById('cvButt')
 var cvBackButton = document.getElementById('goBack')
 var cv = document.getElementById('cv')
+//var body = document.getElementsByTagName('body')
+
 cvButton.addEventListener("click", showCV)
 cvBackButton.addEventListener("click", hideCV)
 function showCV(){
-    cv.style.display = 'flex'
+    cv.style.right = '0'
+    //body.style.filter = 'brightness("50%")'
 }
 function hideCV(){
-    cv.style.display = 'none'
+    cv.style.right = '-1000px'
 }
