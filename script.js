02f190e5e40ffa21aa3638c3a1a2e10ffbf77ab9
Krystian Tomczyk <krynio340@gmail.com>
2022-03-09 22:02:05 +0100

    svg fix, many minor changes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var cvButton = document.getElementById('cvButt')
 var cvBackButton = document.getElementById('goBack')
 var cv = document.getElementById('cv')
-//var body = document.getElementsByTagName('body')
+var body = document.getElementById('parallax')
 
 cvButton.addEventListener("click", showCV)
 cvBackButton.addEventListener("click", hideCV)
 function showCV(){
-    cv.style.right = '0'
-    //body.style.filter = 'brightness("50%")'
+    cv.style.transform = 'translateX(0)';
+    body.style.filter = 'brightness(50%)';
 }
 function hideCV(){
-    cv.style.right = '-1000px'
+    cv.style.transform = 'translateX(100%)';
+    body.style.filter = 'brightness(100%)';
 }
