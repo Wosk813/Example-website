68eb6acb2530d27714cc464bcb577a8fad14b499
Krystian Tomczyk <krynio340@gmail.com>
2022-05-10 19:36:37 +0200

    last commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var cvButton = document.getElementById('cvButt')
 var cvBackButton = document.getElementById('goBack')
 var cv = document.getElementById('cv')
 var background = document.getElementById('background')
+var body = document.querySelector('body')
 
 cvButton.addEventListener("click", showCV)
 cvBackButton.addEventListener("click", hideCV)
@@ -9,9 +10,11 @@ function showCV(){
     cv.classList.add('show')
     cv.classList.remove('hide')
     background.classList.add('background')
+    body.style.overflow = 'hidden'
 }
 function hideCV(){
     cv.classList.add('hide')
     cv.classList.remove('show')
     background.classList.remove('background')
+    body.style.overflowY = 'auto'
 }
